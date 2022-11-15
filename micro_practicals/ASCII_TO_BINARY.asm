58911979bc078337ab9219f0e6367c8a147f93d1
adarsh kumar <adarshkumarjudoka212@gmail.com>
2022-11-15 23:37:19 +0530

    change in asscii

--- micro_practicals/ASCII_TO_BINARY.asm
+++ micro_practicals/ASCII_TO_BINARY.asm
@@ -1,20 +1,24 @@
-.model small
-.data
-MESS0 DB 10,13,"ENTER THE NUMBER:$"
-
- ORG 1000H
- MOV SI,1100H
- MOV DI,1400H
- CLD
- MOV BL,20H
-
-NEXT:LODSB
-    CMP AL,BL
-    JE EXIT
-    SUB AL,30H
-    CMP AL,0AH
-    JC STORE
-    SUB AL,07H
-STORE:STOSB
-    JMP NEXT
-EXIT:HLT
+.model small  
+ .data  
+ a db 0AH  
+ .code  
+         mov      ax, @data      ; Initialize data section  
+      mov      ds, ax  
+      mov      al, a             ; Load number1 in al  
+      mov      cl , 08H  
+      mov      ah, 00h           ; ah=00  
+ up :     shl      al, 01h       ; divide the number by 2   
+                                 ;SHL gives the same result         
+      mov      bl, al                 
+      mov      al, 00H  
+      adc      al, 30h  
+      mov      dl, al  
+      mov      ah, 02h  
+      int      21h  
+      mov      al,bl  
+      dec      cl  
+      jnz      up  
+      mov      ah, 4cH            ; Terminate Program  
+      mov 	   ah,00H
+	int      21H  
+      end
